3b0334c9c8dca36f4e53296edab5deab8551640d
Raunak singh <51930237+tavisca-rsingh@users.noreply.github.com>
2019-06-22 14:41:55 +0530

    Update Program.cs

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -23,7 +23,58 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         public static int FindDigit(string equation)
         {
             // Add your code here.
+            /* Splitting the values  */
+            string[] tok = equation.Split("=");
+            string[] val = tok[0].Split("*");
+            
+           /* Assigning operands */
+            string op1=val[0],op2=val[1],op3=tok[1];
+             int first,second,third;
+             string str;
+            
+            /* Checking if third operand contains "?" */
+            if(op3.Contains("?"))                      
+            { 
+                /* Calculating result */
+               first=Convert.ToInt32(op1);
+               second=Convert.ToInt32(op2);
+               first=first*second;
+               str=first.ToString();
+               return getVal(str,op3);
+            }
+            else 
+            {
+                /* if first operator contains "?" then swap value with second operator */
+                if(op1.Contains("?"))
+                {
+                    string temp=op1;
+                    op1=op2;
+                    op2=temp;
+                }   
+                first=Convert.ToInt32(op1);
+                third=Convert.ToInt32(op3);
+                
+                /* Return -1 if it is not divisible else compute value */
+                
+                if(third%first!=0) return -1;
+                
+                first=third/first;
+                str=first.ToString();
+                return getVal(str,op2);
+            }
+
             throw new NotImplementedException();
+        }
+        /* Finding the missing value */
+        public static int getVal(string a, string b)
+        {
+            if(a.Length!=b.Length) 
+               return -1;
+            for(int i=0;i<a.Length;i++)
+            {
+                if(b[i].Equals('?')) return a[i]-48;
+            }
+            return -1;
         }
     }
 }
